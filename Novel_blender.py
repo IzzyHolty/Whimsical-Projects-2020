7f820e1199079c42c61c8ee6ad8a52d875c40364
Izzy Holt <izzyholty@gmail.com>
2020-04-21 18:47:12 +1000

    added two txt files

---
+++ Novel_blender.py
@@ -0,0 +1,14 @@
+#inspired by Zach Whalen's NanoGenMo Challenge Markovify code - https://www.youtube.com/watch?v=oeL1u7n2d2s
+
+import random
+import markovify
+
+
+def blendNovel(file1, file2):
+    
+
+def main():
+    #user input text1, text2, blendNovel(text1, text2)
+    blendNovel("LobsterQuadrille.txt", "Ozymandias.txt")
+
+main()
