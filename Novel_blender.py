dccf5a5ccd64b0664e49c63e8366866b8ec4ec43
Izzy Holt <izzyholty@gmail.com>
2020-04-21 20:24:30 +1000

    different text

--- Novel_blender.py
+++ Novel_blender.py
@@ -6,9 +6,23 @@ import markovify
 
 def blendNovel(file1, file2):
     
+    with open(file1, encoding="utf8") as f1:
+        text1 = f1.read()
 
+    novel_model = markovify.Text(text1)
+
+    for i in range(5):
+        print(novel_model.make_sentence())
+    
+    #for testing
+    #print(text1)
+
+
+    
 def main():
     #user input text1, text2, blendNovel(text1, text2)
-    blendNovel("LobsterQuadrille.txt", "Ozymandias.txt")
+    blendNovel("SherlockHolmes.txt", "AliceInWonderland.txt")
+
+
 
 main()
